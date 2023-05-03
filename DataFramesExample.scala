1ce66ef901d52e2dd4b47a8dc3547f7e061d06fd
Vishakha Balasaheb Jadhav <61991482+vishakha1111@users.noreply.github.com>
2023-05-03 17:48:21 +0530

    Orders Data

---
+++ DataFramesExample.scala
@@ -0,0 +1,36 @@
+import java.security.Timestamp
+
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+case class OrdersData(order_id: Int, order_date: Timestamp, order_customer_id: Int, order_status:String)
+
+object DataFramesExample extends App{
+  
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  val sparkConf = new SparkConf()
+  sparkConf.set("spark.app.name", "My first application")
+  sparkConf.set("spark.master", "local[2]")
+  
+  val spark = SparkSession.builder()
+  .config(sparkConf)
+  .getOrCreate()
+  
+  val ordersDf = spark.read
+  .option("header", true)
+  .option("inferSchema", true)
+  .csv("D:/VISHAKHA/BIG DATA COURSE/Week 11/orders.csv")
+  
+  import spark.implicits._
+  val ordersDS = ordersDf.as[OrdersData]
+  
+  ordersDS.filter(x => x.order_id < 10)
+  //ordersDf.filter("order_ids < 10").show()
+  //Logger.getLogger(getClass.getName).info("My application has ran successfully")
+  ordersDf.printSchema()
+  
+  scala.io.StdIn.readLine()
+  spark.stop()
+}
